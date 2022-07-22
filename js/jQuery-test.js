94f48bf404c590e37f61ef80471cbc6c494d10a2
manndoreiku2006302 <108842876+manndoreiku2006302@users.noreply.github.com>
2022-07-22 09:26:56 +0900

    Add files via upload

---
+++ js/jQuery-test.js
@@ -0,0 +1,11 @@
+alert('サイトを開いてくれてありがとうございます!!');
+
+$('#uliki').on('mouseenter',function(){
+    console.log('ウィキペディア “宮崎県”が注目されました.');
+
+})
+
+$('#home').on('mouseenter',function(){
+    console.log('宮崎県ホームページが注目されました.');
+
+})
